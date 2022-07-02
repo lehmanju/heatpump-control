2a2691136eedb06b2e7d2885a5916a0a2f9af0a7
Julius <git@devpi.de>
2022-07-02 21:10:48 +0200

    working version

--- heatpump-control.py
+++ heatpump-control.py
@@ -1,21 +1,25 @@
 import paho.mqtt.client as mqtt
 import time
+import math
 
 target_current = 0
 current_power = 0
 current_temp = 0
 
-def on_message(client, useradata, message):
+def on_message(client, _userdata, message):
     global target_current
     global current_power
-    if message.topic == "evcc/loadpoints/2/maxCurrent":
+    global current_temp
+    if message.topic == "heatpump_control/maxCurrent":
         target_current = int(message.payload)
-    else:
+    elif message.topic == "panasonic_heat_pump/s0/Watt/1":
         current_power = int(message.payload)
+    else:
+        current_temp = int(message.payload)
 
 broker = "car.devpi.de"
 
-client = mqtt.Client("heatpump_control")
+client = mqtt.Client("heatpump_control_client")
 client.connect(broker)
 
 client.loop_start()
@@ -24,6 +28,7 @@ client.loop_start()
 # evcc target power = evcc controlled power value that heatpump should reach
 client.subscribe("heatpump_control/maxCurrent")
 client.subscribe("panasonic_heat_pump/s0/Watt/1")
+client.subscribe("panasonic_heat_pump/main/Main_Outlet_Temp")
 
 client.on_message = on_message
 
@@ -31,8 +36,14 @@ client.on_message = on_message
 kp = 0.1
 
 while True:
-    target_power = target_current * 220 # P = U * I
-    u = kp * (target_current-current_power)
-    client.publish("base_topic/commands/SetZ1HeatRequestTemperature", u + current_temp)
-    client.publish("base_topic/commands/SetZ2HeatRequestTemperature", u + current_temp)
-    time.sleep(20)
+    target_power = target_current * 230 # P = U * I
+    u = math.trunc(kp * (target_power-current_power))
+    new_temp = u + current_temp
+    if new_temp > 60:
+        new_temp = 60
+    if new_temp < 30:
+        new_temp = 30
+    print("new temperature: ", new_temp)
+    client.publish("panasonic_heat_pump/commands/SetZ1HeatRequestTemperature", new_temp )
+    # client.publish("base_topic/commands/SetZ2HeatRequestTemperature", u + current_temp)
+    time.sleep(5)
